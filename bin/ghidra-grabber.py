2bc86a485dd0453d5d9abf37b565c4cf754c96c8
Andrew Powers-Holmes <aholmes@omnom.net>
2023-01-05 22:28:09 +1100

    ghidra-grabber: rewrite for resiliency and 10.2 support

--- bin/ghidra-grabber.py
+++ bin/ghidra-grabber.py
@@ -1,147 +1,115 @@
 #!/usr/bin/env python3
 
 import argparse
-import glob
-import os
 import shutil
-import stat
-import subprocess
 import sys
 import tempfile
-import requests
+from os import PathLike
 from pathlib import Path
+from zipfile import ZipFile
 
-import plistlib
+import requests
+
+TEMP_DIR = Path(tempfile.gettempdir())
 
 
-def getReleases():
-    r = requests.get('https://api.github.com/repos/nationalsecurityagency/ghidra/releases')
-    releases = r.json()
-    releases = sorted(releases, key=lambda x: x["created_at"], reverse=True)
+def get_releases() -> list:
+    res = requests.get("https://api.github.com/repos/nationalsecurityagency/ghidra/releases")
+    res.raise_for_status()
+    releases = sorted(res.json(), key=lambda x: x["created_at"], reverse=True)
     return releases
 
-def listVersions():
-    releases = getReleases()
-    for release in releases:
+
+def list_releases() -> None:
+    for release in get_releases():
         vers = release["name"].split(" ")[1]
         print(f"\t{vers}")
 
 
-parser = argparse.ArgumentParser()
-parser.add_argument('-u', '--url', help='Ghidra zip URL. Defaults to latest from Github')
-parser.add_argument(
-        '-p', '--path', help='Path to Ghidra zip or install', type=Path)
-parser.add_argument('-v', '--version', dest='version', required=False,
-                    help='Set the version for the dmg. Eg: "9.1 BETA"')
-parser.add_argument('--extension', type=Path, nargs='*', help='Path to a Ghidra extension zip to install')
-parser.add_argument('--list-versions', action='store_true', help='Print available Ghidra versions')
-
-parser.add_argument('OUTPUT', type=Path, help="Path to install Ghidra to")
-
-args = parser.parse_args()
-
-if args.version == 'latest':
-    args.version = None
-
-if args.list_versions:
-    print("[+] Available Ghidra versions from Github:")
-    listVersions()
-    exit(0)
-
-if args.OUTPUT.is_dir():
-    print(f"[!!] Directory already exists at {args.OUTPUT}.")
-    exit(1)
-
-with tempfile.TemporaryDirectory() as tmp_dir:
-    ghidra_content = None
-    ghidra_zip_name = None
-    dest_path = Path(tmp_dir)
-
-    if not args.url and not args.path and not args.version:
-        print("[!] No URL or path provided, getting latest from github")
-        releases = getReleases()
-        release = releases[0] # The latest release
-        args.version = release["name"].split(" ")[1]
-        args.url = release["assets"][0]["browser_download_url"]
-        print(f"[+] Fetching {args.version} from {args.url}")
-    elif args.version and not args.url and not args.path:
-        # If we have a version and not a path or URL, we'll get that specific release
-        releases = getReleases()
-        for release in releases:
-            if args.version in release["name"]:
-                # Found it
-                args.url = release["assets"][0]["browser_download_url"]
-                print(f"[+] Found version {args.version} on Github @ {args.url}")
-        if not args.url:
-            print(f"[!] Failed to find version {args.version} on Github. Found:")
-            listVersions()
-            exit(1)
+def download_file(url: str, outdir: PathLike = TEMP_DIR) -> Path:
+    outdir = Path(outdir) if not isinstance(outdir, Path) else outdir
+    outfile = outdir.joinpath(Path(url).name)
+    with requests.get(url, stream=True) as r:
+        r.raise_for_status()
+        with outfile.open("wb") as f:
+            for chunk in r.iter_content(chunk_size=32768):
+                f.write(chunk)
+    return outfile
 
 
-    if args.path:
-        print("[-] Will use Ghidra from {}".format(args.path))
-        ghidra_zip_name = Path(args.path).name
-    elif args.url:
-        print("[+] Downloading {}".format(args.url))
-        download = requests.get(args.url)
-        ghidra_zip_name = Path(args.url).name
-
-        if download.status_code == 200:
-            ghidra_content = download.content
-        else:
-            print('[!] Failed to download!')
-            sys.exit(1)
+def download_release(version: str = "latest") -> Path:
+    releases = get_releases()
+    for release in releases:
+        if version in release["name"]:
+            for release_asset in release["assets"]:
+                if version in release_asset["name"]:
+                    asset = release_asset
+                    url = asset["browser_download_url"]
+                    print(f"[+] Found Ghidra {version} on Github @ {url}, downloading...")
+                    return download_file(url, TEMP_DIR)
+            else:
+                print(f"[!] Found Ghidra {version} on Github, but no matching asset! Found:")
+                print(f"\t{[x['name'] for x in release['assets']]}")
+                raise ValueError(f"Failed to find Ghidra {version} release asset")
     else:
-        print("[!] Neither path nor url were specified!")
-        sys.exit(1)
+        print(f"[!] Failed to find Ghidra {version} on GitHub! Found:")
+        print(f"\t{[x['name'] for x in releases]}")
+        raise ValueError(f"Failed to find Ghidra {version} release")
 
-    # calculate the name from a path
-    if ghidra_zip_name:
-        version = ghidra_zip_name.split('ghidra_')[1].split('_')[0]
+
+def install_ghidra(zip_file: PathLike, destdir: PathLike) -> None:
+    destdir = Path(destdir) if not isinstance(destdir, Path) else destdir
+    zip_file = Path(zip_file) if not isinstance(zip_file, Path) else zip_file
+
+    if not zip_file.exists():
+        print(f"[!] Failed to find Ghidra zip @ {zip_file}")
+        raise FileNotFoundError(f"Failed to find Ghidra zip @ {zip_file}")
+
+    print(f"[+] Unpacking {zip_file} to {TEMP_DIR}...")
+
+    with ZipFile(zip_file, "r") as zip:
+        zip.extractall(TEMP_DIR)
+
+    # delete downloaded zip file
+    zip_file.unlink()
+
+    ghidra_dir = TEMP_DIR / zip_file.name[: zip_file.name.find("_PUBLIC") + 7]
+
+    print(f"[+] Moving {zip_file} into destination {destdir}...")
+    if destdir.exists():
+        print(f"[!] Destination {destdir} already exists, removing...")
+        try:
+            destdir.rmdir()
+        except OSError:
+            print(f"[!] Failed to remove {destdir}! Is it a non-empty directory?")
+            raise
+    shutil.move(ghidra_dir.resolve(), destdir.resolve())
+    print(f"[+] Ghidra installed to {destdir}!")
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-u", "--url", help="Ghidra zip URL. Defaults to latest from Github", type=str)
+parser.add_argument("-p", "--path", help="Path to install Ghidra to", type=Path)
+parser.add_argument("-l", "--list", help="List available releases", action="store_true", default=False)
+parser.add_argument("-v", "--version", help="Ghidra version to install. Defaults to latest", type=str)
+
+if __name__ == "__main__":
+    args = parser.parse_args()
+
+    if args.list:
+        list_releases()
+        sys.exit(0)
+
     if args.version:
-        version = args.version
-    
-
-    if args.url:
-       print("[+] Extracting...")
-       with tempfile.TemporaryDirectory() as zip_dir:
-           zip_path = os.path.join(zip_dir, 'Ghidra.zip')
-           with open(zip_path, 'wb') as f:
-               f.write(ghidra_content)
-           subprocess.run(f'unzip -d "{dest_path}" "{zip_path}"', shell=True)
-       print("[+] Extracted to {}".format(dest_path))
+        print(f"[+] Downloading Ghidra {args.version}...")
+        zip_file = download_release(args.version)
+    else:
+        print("[+] Downloading Ghidra 'latest'...")
+        zip_file = download_release()
 
     if args.path:
-        if args.path.is_file():
-            print("[+] Opening {}".format(args.path))
-            with open(args.path, 'rb') as f:
-                ghidra_content = f.read()
-            print("[+] Extracting...")
-            with tempfile.TemporaryDirectory() as zip_dir:
-                zip_path = os.path.join(zip_dir, 'Ghidra.zip')
-                with open(zip_path, 'wb') as f:
-                    f.write(ghidra_content)
-                subprocess.run(f'unzip -d "{dest_path}" "{zip_path}"', shell=True)
-            print("[+] Extracted to {}".format(dest_path))
-        elif args.path.is_dir():
-            print("[+] Copying...")
-            shutil.copytree(args.path, dest_path / args.path.name)
-            print("[+] Copied to {}".format(dest_path / args.path.name))
-
-    # Install any extensions
-    ghidra_install_dir = None
-    try:
-        ghidra_install_dir = next(dest_path.glob(f'ghidra_{version}*'))
-    except IndexError as e:
-        raise Exception(f"Ghidra was not installed into {dest_path}, is the Ghidra zip correctly structured?", e)
-    # If we don't have an install dir matching this format, the launch
-    # script won't work, so we might as well die here
-    extension_dir = ghidra_install_dir.joinpath("Ghidra", "Extensions")
-    if args.extension:
-        for extension in args.extension:
-            print("[+] Installing extension: {extension}")
-            subprocess.run(f'unzip -d "{extension_dir}" "{extension}"', shell=True)
-
-    if args.OUTPUT:
-        shutil.copytree(ghidra_install_dir, args.OUTPUT)
-        print(f"[+] Saved to {args.OUTPUT}")
+        install_path = Path(args.path).resolve()
+    else:
+        install_path = Path.cwd().joinpath("ghidra")
+    print(f"[+] Installing Ghidra to {args.path}...")
+    install_ghidra(zip_file, args.path)
